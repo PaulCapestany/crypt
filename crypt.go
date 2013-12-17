592af539498c1c3155a0d97147ee4b1f73fd9965
Paul Capestany <capestany@gmail.com>
2013-12-17 14:51:28 -0800

    Prettier output using ANSI encoding.

--- crypt.go
+++ crypt.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	// "github.com/daviddengcn/go-colortext"
 	"os"
 	"text/tabwriter"
 	// "io/ioutil"
@@ -16,6 +17,34 @@ var m = flag.Int("m", 7, "mod")
 
 var w = new(tabwriter.Writer)
 
+// ANSI color control escape sequences.
+// Shamelessly copied from https://github.com/sqp/godock/blob/master/libs/log/colors.go
+var (
+	reset      = "\x1b[0m"
+	bright     = "\x1b[1m"
+	dim        = "\x1b[2m"
+	underscore = "\x1b[4m"
+	blink      = "\x1b[5m"
+	reverse    = "\x1b[7m"
+	hidden     = "\x1b[8m"
+	fgBlack    = "\x1b[30m"
+	fgRed      = "\x1b[31m"
+	fgGreen    = "\x1b[32m"
+	fgYellow   = "\x1b[33m"
+	fgBlue     = "\x1b[34m"
+	fgMagenta  = "\x1b[35m"
+	fgCyan     = "\x1b[36m"
+	fgWhite    = "\x1b[37m"
+	bgBlack    = "\x1b[40m"
+	bgRed      = "\x1b[41m"
+	bgGreen    = "\x1b[42m"
+	bgYellow   = "\x1b[43m"
+	bgBlue     = "\x1b[44m"
+	bgMagenta  = "\x1b[45m"
+	bgCyan     = "\x1b[46m"
+	bgWhite    = "\x1b[47m"
+)
+
 // var secrets = map[string]string{}
 
 // func ReadSecrets() {
@@ -28,15 +57,22 @@ func modSeries(base int, exponent int, mod int) {
 	// and at least three blanks of padding (so wider column entries do not
 	// touch each other).
 	w.Init(os.Stdout, 5, 0, 3, ' ', tabwriter.AlignRight)
-	fmt.Fprintf(w, "Equation\tResult\tMod %v\t\n", mod)
-	fmt.Fprintf(w, "--------\t------\t------\t\n")
+	fmt.Fprintf(w, underscore+bright+"Equation\tResult\tMod %v\t"+reset+"\n", mod)
+	// fmt.Fprintf(w, "--------\t------\t------\t\n")
 
 	value := base
 	for i := 1; i <= exponent; i++ {
 		modVal := value % mod
-		fmt.Fprintf(w, "%v^%v\t%v\t%v\t\n", base, i, value, modVal)
+
+		if i%2 == 0 {
+			fmt.Fprintf(w, underscore+bright+"%v^%v\t%v\t%v\t"+reset+"\n", base, i, value, modVal)
+		} else {
+			fmt.Fprintf(w, underscore+dim+"%v^%v\t%v\t%v\t"+reset+"\n", base, i, value, modVal)
+		}
 		value *= base
+		// ct.ResetColor()
 	}
+	w.Flush()
 }
 
 func main() {
@@ -44,5 +80,4 @@ func main() {
 	flag.Parse()
 
 	modSeries(*b, *e, *m)
-	w.Flush()
 }
