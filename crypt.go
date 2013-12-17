009de71035f55be421b2067c8d543ba39845f63e
Paul Capestany <capestany@gmail.com>
2013-12-17 13:34:05 -0800

    Exponents + mod + tabbed output.

--- crypt.go
+++ crypt.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"text/tabwriter"
 	// "io/ioutil"
 	// "encoding/json"
 	// "github.com/____/____"
 )
+
+var b = flag.Int("b", 2, "base")
+var e = flag.Int("e", 3, "exponent")
+var m = flag.Int("m", 7, "mod")
+
+var w = new(tabwriter.Writer)
 
 // var secrets = map[string]string{}
 
@@ -14,7 +23,26 @@ import (
 // 	json.Unmarshal(someBytes, &secrets)
 // }
 
-func main() {
-	fmt.Println("crypt says hello!")
+func modSeries(base int, exponent int, mod int) {
+	// Format right-aligned in space-separated columns of minimal width 5
+	// and at least one blank of padding (so wider column entries do not
+	// touch each other).
+	w.Init(os.Stdout, 5, 0, 3, ' ', tabwriter.AlignRight)
+	fmt.Fprintf(w, "Equation\tResult\tMod %v\t\n", mod)
+	fmt.Fprintf(w, "--------\t------\t------\t\n")
+
+	value := base
+	for i := 0; i <= exponent; i++ {
+		value *= base
+		modVal := value % mod
+		fmt.Fprintf(w, "%v^%v\t%v\t%v\t\n", base, i, value, modVal)
+	}
 }
 
+func main() {
+	fmt.Println("crypt says hello!\n")
+	flag.Parse()
+
+	modSeries(*b, *e, *m)
+	w.Flush()
+}
